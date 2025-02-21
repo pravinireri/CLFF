042159161ba51ed7507d3325e92951df08bd95e4
pravinireri <pkireri@gmail.com>
2025-02-21 11:48:42 +0300

    minor updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,28 @@
 // Copyright Year
     document.getElementById("year").textContent = new Date().getFullYear();
+
+// Latest Video
+const channelId = 'UCRuGL0Y3BNP1DlcW_TWgGOQ';
+
+const apiKey = 'AIzaSyA1rRSuwkQ2nP7XFQptMQh2kZG86orPZyU';
+
+async function fetchLatestVideo() {
+    try {
+        const response = await fetch(`https://www.googleapis.com/youtube/v3/search?key=${apiKey}&channelId=${channelId}&order=date&part=snippet&maxResults=1`);
+        const data = await response.json();
+
+        if (data.items.length > 0) {
+            const latestVideo = data.items[0];
+            const videoId = latestVideo.id.videoId;
+            const videoUrl = `https://www.youtube.com/embed/${videoId}`;
+
+            document.getElementById('video-frame').src = videoUrl;
+        } else {
+            console.error('No videos found for the channel.');
+        }
+    } catch (error) {
+        console.error('Error fetching latest video:', error);
+    }
+}
+
+window.onload = fetchLatestVideo;
